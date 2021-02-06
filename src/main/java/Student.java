c564cb181d8afec040a6b69f73e4fdfbfeab9abc
Rysard Poplavskij <rpoplavskij@gmail.com>
2021-02-06 16:36:14 +0300

    First commit

---
+++ src/main/java/Student.java
@@ -0,0 +1,51 @@
+public class Student {
+    private String name;
+    private String secondName;
+
+    public Student(String name, String secondName) {
+        this.name = name;
+        this.secondName = secondName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Student student = (Student) o;
+
+        if (name != null ? !name.equals(student.name) : student.name != null) return false;
+        return secondName != null ? secondName.equals(student.secondName) : student.secondName == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + (secondName != null ? secondName.hashCode() : 0);
+        return result;
+    }
+
+    public static Student createStudent() {
+        Student student1 = new Student(RandomNames.getRandomName(), RandomNames.getRandomSecondName());
+        return student1;
+    }
+
+
+
+}
