9faac260e0bac34faffd1025a04423211650fd6b
Rysard Poplavskij <rpoplavskij@gmail.com>
2021-02-15 11:38:13 +0300

    delete nonused classes

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,20 +1,12 @@
 import java.util.ArrayList;
-import java.util.Arrays;
+
 
 public class MainClass {
 
-    static ArrayList<Student> studentsList = new ArrayList<Student>();
+
 
     public static void main(String[] args) {
 
-        for (int i = 0; i <= 1000; i++) {
-            studentsList.add(Student.createStudent());
-        }
-
-        for (int i = 0; i < studentsList.size(); i++) {
-            System.out.println("name: " + studentsList.get(i).getName() +
-                    ", second name: " + studentsList.get(i).getSecondName());
-        }
 
 
     }
